a82fe74f50ab7d368fdd915773db06fd1075f58a
g <gharnett@gmail.com>
2018-11-05 03:19:23 -0500

    login and register form styling

--- react-thoughts-client/src/components/LoginForm.jsx
+++ react-thoughts-client/src/components/LoginForm.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
 
 const LoginForm = (props) => {
-
-return (
-    <div>
-<form>
-    <label htmlFor="email">Email: </label>
-    <br />
-        <input
-        name="email"
-        id="email"
-        type="email"
-        value={props.email.value}
-        onChange={props.handleChange}
-        />
-        <br /><br />
-        <label htmlFor="password">Password:</label>
-        <br />
-        <input
-        name="password"
-        id="password"
-        type="password"
-        value={props.password.value}
-        onChange={props.handleChange}
-        />
-        <br />
-        
-</form>
+    return (
+        <div className="form-container">
+            <div className="login">
+            { props.isRegister ? <h2>Register</h2> : null }
+            { props.isLogin ? <h2>Login</h2> : null }
+                
+            </div>
+            <form>
+            <div className="row">
+                <div className="col-25">
+                    <label for="email" className="login">email</label>
+                </div>
+                <div className="col-75">
+                    <input
+                    name="email"
+                    id="email"
+                    type="email"
+                    value={props.email.value}
+                    onChange={props.handleChange}
+                    />
+                </div>
+            </div> 
+            <div className="row">
+                <div className="col-25">
+                    <label for="password" className="login">password</label>
+                </div>
+                <div className="col-75">
+                    <input
+                    name="password"
+                    id="password"
+                    type="password"
+                    value={props.password.value}
+                    onChange={props.handleChange}
+                    />
+                </div>
+            </div>        
+            </form>
 {
 props.isRegister ? 
 <button onClick={props.register}>Register</button>
